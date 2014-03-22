6b7e58dbae96e46692da9d53975d4d6275ad9e06
Nishan Bajracharya <nishan_cd@ymail.com>
2014-03-22 17:46:28 +0545

    Popup for toolbar

--- template/footer.php
+++ template/footer.php
@@ -3,8 +3,9 @@
 	Looma! Re-discovering Education.
 	<center style="margin-top:-17.5px">
 		<div class="toolbar">
+			<span class="caption">Home</span>
 			<button class="selected"><img src="/template/images/icon0.png" alt="Home"></button>
-			<button><img src="/template/images/icon1.png" alt="Games"></button>
+			<button><img src="/template/images/icon1.png" alt="Fun and Games"></button>
 		</div>
 	</center>
 </footer>
--- template/scripts/main.js
+++ template/scripts/main.js
@@ -2,5 +2,16 @@ $(function(){
 	$(".toolbar button").click(function(){
 		$(".toolbar button").removeClass("selected");
 		$(this).addClass("selected");
+		switch($(this).index()){
+			case 1:
+			window.location.href = "/"
+			break;
+		}
+	})
+	$(".toolbar button").hover(function(){
+		$(".toolbar .caption").text($(this).find('img').attr('alt'));
+		capWid = $(".toolbar .caption").width()-8;
+		$(".toolbar .caption").css("left",(($(document).width()-capWid)/2)+"px");
+		$(".toolbar .caption").toggle();
 	})
 })
